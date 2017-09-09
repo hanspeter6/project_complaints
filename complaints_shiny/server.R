ac7ecac4b686ceb73cdea7067ae64e2b008ad5ed
hanspeter6 <h.bakker@ru.ac.za>
2017-09-09 07:28:57 +0200

    commit after mess up from desktop

--- complaints_shiny/server.R
+++ complaints_shiny/server.R
@@ -1,12 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
 library(tidytext)
@@ -21,130 +12,131 @@ complaints_dtm <- readRDS("complaints_dtm.rds")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$histPlot <- renderPlot({
-    
-    # generate histograms of total sentiment created input$product from ui.R
-          
-          # product select
-          if(input$product == "All") {
-                  x <- complaints_sentiments
-          }
-          else {
-                  x <- complaints_sentiments %>%
-                          filter(product == input$product)  
-          }
-          
-          # # compensation or not
-          # if(input$compensation == "Yes & No") {
-          #         x <- x 
-          # }
-          # else if(input$compensation == "Yes") {
-          #         x <- x %>%
-          #                 filter(consumer_compensated == TRUE)
-          # }
-          # else if(input$compensation == "No") {
-          #         x <- x %>%
-          #                 filter(consumer_compensated == FALSE)
-          # }
-          
-          # dealing with date range
-          x <- x %>%
-                  filter(date_received > input$dates[1] & date_received < input$dates[2])
-
-    ggplot(x, aes(tot_sentiment, fill = consumer_compensated)) + 
-            geom_histogram(binwidth = 2) +
-            xlab("Total Sentiment") +
-            ylab("Complaints") +
-            scale_fill_discrete(name="compensated",
-                                labels=c("No", "Yes")) +
-            xlim(c(-20,20))
-    
-  })
-  
-  # output$linePlot <- renderPlot({
-  #         
-  #         
-  #         
-  # })
-  
-  complaints_lda <- reactive({
-          set.seed(56)
-          LDA(complaints_dtm, k = input$k)
-  })
-  
-  complaints_topics <- reactive({
-          tidy(complaints_lda(), matrix = "beta")%>%
-                  group_by(topic) %>%
-                  top_n(15, beta) %>%
-                  ungroup() %>%
-                  arrange(topic, -beta) %>%
-                  mutate(term = reorder(term, beta))
-          
-          
-  })
-  
-  output$topicPlot <- renderPlot({
-
-          ggplot(complaints_topics(), aes(term, beta, fill = factor(topic))) +
-                  geom_col(show.legend = FALSE) +
-                  facet_wrap(~ topic, scales = "free") +
-                  coord_flip()
-          
-  })
-  
-  
-  output$myText <- renderText({
-          
-          # display my textinput
-          print(input$text)
-          
-  })
-  
-  output$sentiment <- renderText({
-          
-          # Sentiment score
-          
-          # create table:
-          tab <- data.frame(id = 21000, complaint = input$text)
-          tab$complaint <- as.character(tab$complaint)
-          
-          # a word unigram list of words
-          my_list <- unnest_tokens(tab, output = words, input = complaint, token = "words")
-          
-          # get rid of stopwords
-          my_list2 <- my_list %>%
-                  filter(!words %in% stopwords(), str_detect(words, "[a-z]"))
-          
-          # add sentiment score column to the word_list frame
-          
-          my_sentiments <- sentiments %>%
-                  filter(lexicon == "bing") %>%
-                  mutate(score = ifelse(sentiment == "positive", 1, -1)) %>%
-                  select(word, sentiment, score)
-          
-          words_list_sentiment <- my_list2 %>%
-                  left_join(my_sentiments, by = c("words" = "word")) %>%
-                  filter(!is.na(sentiment)) %>%
-                  group_by(id) %>%
-                  mutate(tot_sentiment = sum(score)) %>%
-                  ungroup()
-          
-          id_scores <- words_list_sentiment %>%
-                  group_by(id) %>%
-                  summarise(tot_sentiment = mean(tot_sentiment))
-          
-          # now want the quantile of the score of the new complaint:
-          
-          percentile <- ecdf(complaints_sentiments$tot_sentiment)
-          target <- percentile(id_scores$tot_sentiment)
-          
-          out <- c(as.character(id_scores$tot_sentiment), as.character(target))
-          print(out)
- 
-          # now want to get topic probabilities..(gamma) for the my new complaint
-          #  fuck...
-  })
-  
-  
+        
+        output$histPlot <- renderPlot({
+                
+                # generate histograms of total sentiment created input$product from ui.R
+                
+                # product select
+                if(input$product == "All") {
+                        x <- complaints_sentiments
+                }
+                else {
+                        x <- complaints_sentiments %>%
+                                filter(product == input$product)  
+                }
+                
+                # # compensation or not
+                # if(input$compensation == "Yes & No") {
+                #         x <- x 
+                # }
+                # else if(input$compensation == "Yes") {
+                #         x <- x %>%
+                #                 filter(consumer_compensated == TRUE)
+                # }
+                # else if(input$compensation == "No") {
+                #         x <- x %>%
+                #                 filter(consumer_compensated == FALSE)
+                # }
+                
+                # dealing with date range
+                x <- x %>%
+                        filter(date_received > input$dates[1] & date_received < input$dates[2])
+                
+                ggplot(x, aes(tot_sentiment, fill = consumer_compensated)) + 
+                        geom_histogram(binwidth = 2) +
+                        xlab("Total Sentiment") +
+                        ylab("Complaints") +
+                        scale_fill_discrete(name="compensated",
+                                            labels=c("No", "Yes")) +
+                        xlim(c(-20,20))
+                
+        })
+        
+        # output$linePlot <- renderPlot({
+        #         
+        #         
+        #         
+        # })
+        
+        
+        complaints_lda <- reactive({
+                set.seed(56)
+                LDA(complaints_dtm, k = input$k)
+        })
+        
+        complaints_topics <- reactive({
+                tidy(complaints_lda(), matrix = "beta")%>%
+                        group_by(topic) %>%
+                        top_n(15, beta) %>%
+                        ungroup() %>%
+                        arrange(topic, -beta) %>%
+                        mutate(term = reorder(term, beta))
+                
+                
+        })
+        
+        output$topicPlot <- renderPlot({
+                
+                ggplot(complaints_topics(), aes(term, beta, fill = factor(topic))) +
+                        geom_col(show.legend = FALSE) +
+                        facet_wrap(~ topic, scales = "free") +
+                        coord_flip()
+                
+        })
+        
+        
+        output$myText <- renderText({
+                
+                # display my textinput
+                print(input$text)
+                
+        })
+        
+        output$sentiment <- renderText({
+                
+                # Sentiment score
+                
+                # create table:
+                tab <- data.frame(id = 21000, complaint = input$text)
+                tab$complaint <- as.character(tab$complaint)
+                
+                # a word unigram list of words
+                my_list <- unnest_tokens(tab, output = words, input = complaint, token = "words")
+                
+                # get rid of stopwords
+                my_list2 <- my_list %>%
+                        filter(!words %in% stopwords(), str_detect(words, "[a-z]"))
+                
+                # add sentiment score column to the word_list frame
+                
+                my_sentiments <- sentiments %>%
+                        filter(lexicon == "bing") %>%
+                        mutate(score = ifelse(sentiment == "positive", 1, -1)) %>%
+                        select(word, sentiment, score)
+                
+                words_list_sentiment <- my_list2 %>%
+                        left_join(my_sentiments, by = c("words" = "word")) %>%
+                        filter(!is.na(sentiment)) %>%
+                        group_by(id) %>%
+                        mutate(tot_sentiment = sum(score)) %>%
+                        ungroup()
+                
+                id_scores <- words_list_sentiment %>%
+                        group_by(id) %>%
+                        summarise(tot_sentiment = mean(tot_sentiment))
+                
+                # now want the quantile of the score of the new complaint:
+                
+                percentile <- ecdf(complaints_sentiments$tot_sentiment)
+                target <- percentile(id_scores$tot_sentiment)
+                
+                out <- c(as.character(id_scores$tot_sentiment), as.character(target))
+                print(out)
+                
+                # now want to get topic probabilities..(gamma) for the my new complaint
+                #  fuck...
+        })
+        
+        
 })
--- complaints_shiny/ui.R
+++ complaints_shiny/ui.R
@@ -9,56 +9,49 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Complaints"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-        helpText("Select a Product"),
-        selectInput(inputId = "product",
-                    label = "Select Product:",
-                    choices = c("All", "Mortgage", "Credit card", "Debt collection", "Credit reporting", "Bank account or service"),
-                    selected = "All"),
-            
-        helpText("Select a Date Range"),
-        dateRangeInput(inputId = "dates",
-                       label = "Date Range:",
-                       start =  "2015-03-19",
-                       end = "2016-04-20",
-                       min = "2015-03-19",
-                       max = "2016-04-20"),
-        
-        helpText("Select a Number of Topics"),
-        numericInput(inputId = "k",
-                     label = "Number of Topics:",
-                     value = 2,
-                     min = 2,
-                     max = 5,
-                     step = 1),
-        
-        helpText("Test a Sample Complaint Here"),    
-        textInput(inputId = "text",
-                  label = "Write your Complaint here:",
-                  width = 'auto'),
-        
-        helpText("To Submit your Selections"),
-        submitButton("Apply Changes", icon("refresh"))),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-            tabsetPanel(
-                    tabPanel("Sentiment", plotOutput("histPlot")),
-                    tabPanel("Topic", plotOutput("topicPlot")),
-                    tabPanel("Sample",
-                             verbatimTextOutput("myText"),
-                             helpText("Sentiment Score $ Percentile"),
-                             textOutput("sentiment"))
-            )
-    )
-
-  )
+shinyUI(navbarPage("My Application",
+                   tabPanel("Sentiment Analysis",
+                            sidebarLayout(
+                                    sidebarPanel(
+                                            selectInput(inputId = "product",
+                                                        label = "Select Product:",
+                                                        choices = c("All", "Mortgage", "Credit card", "Debt collection", "Credit reporting", "Bank account or service"),
+                                                        selected = "All"),
+                                            dateRangeInput(inputId = "dates",
+                                                           label = "Date Range:",
+                                                           start =  "2015-03-19",
+                                                           end = "2016-04-20",
+                                                           min = "2015-03-19",
+                                                           max = "2016-04-20"),
+                                            submitButton("Apply Changes", icon("refresh"))),
+                                    mainPanel(
+                                            plotOutput("histPlot")
+                                    ))),
+                   
+                   tabPanel("Topic Analysis",
+                            sidebarLayout(
+                                    sidebarPanel(
+                                            numericInput(inputId = "k",
+                                                         label = "Number of Topics:",
+                                                         value = 2,
+                                                         min = 2,
+                                                         max = 5,
+                                                         step = 1),
+                                            submitButton("Apply Changes", icon("refresh"))),
+                                    mainPanel(
+                                            plotOutput("topicPlot")
+                                    ))),
+                   
+                   tabPanel("Sample Analysis",
+                            sidebarLayout(
+                                    sidebarPanel(
+                                            textInput(inputId = "text",
+                                                      label = "Write your Complaint here:",
+                                                      width = 'auto'),
+                                            submitButton("Apply Changes", icon("refresh"))),
+                                    mainPanel(
+                                            verbatimTextOutput("myText"),
+                                            textOutput("sentiment")
+                                    )))
 ))
+
